cd090da5eb32cdd59963f52f527cc24c79990fb8
LuisMata <mata.ramirez.1iv14@gmail.com>
2023-03-12 13:20:20 -0600

    ajh

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://LuisMata12.github.io/rickandmorty'
+  base:'https://LuisMata12.github.io/rickandmorty/'
 })
